9884b03a9b679e3f7ee0b8f500ad98649bbec097
Derek McTavish <derek@mctavishdynamics.com>
2026-09-18 02:54:53 +0700

    editors: pin grammar to df6672820fd8

--- editors/vim/lua/piton/init.lua
+++ editors/vim/lua/piton/init.lua
@@ -31,7 +31,7 @@ function M.setup(opts)
     parsers.get_parser_configs().piton = {
       install_info = {
         url = opts.grammar_url or 'git@github.com:piton-lang/tree-sitter-piton.git',
-        revision = opts.grammar_revision or 'ac3aaa7734be4c7975512887e5d45390001aa5d4',
+        revision = opts.grammar_revision or 'df6672820fd815429372fc34e5092805d7af4b4a',
         files = { 'src/parser.c' },
       },
       filetype = 'piton',
